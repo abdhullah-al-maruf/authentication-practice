d1affc68730389ef4e6c6de305ab84814772f102
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-04-29 18:04:57 +0600

    signup funtionality added with better auth

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { authClient } from "@/lib/auth-client"; //import the auth client
 import { Check } from "@gravity-ui/icons";
 import {
   Button,
@@ -10,15 +10,54 @@ import {
   Label,
   TextField,
 } from "@heroui/react";
+import { useRouter } from "next/navigation";
 
 const SignUp = () => {
+  const router = useRouter(); // this hook use for redirect to the home page
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    // step-2 now get the value from the from as target.name.value
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    // now copy the code for signup from better auth for send the data into mongodb
+    // and this way data will send to the db
+    const { data, error } = await authClient.signUp.email(
+      {
+        email,
+        password,
+        name,
+      },
+      {
+        onRequest: (ctx) => {
+          // show loading
+          <p>loading.....</p>;
+        },
+        onSuccess: (ctx) => {
+          // redirect to the dashboard or sign in page
+          router.push("/");
+        },
+        onError: (ctx) => {
+          // display the error message
+          alert(ctx.error.message);
+        },
+      },
+    );
+
+    // console.log(data, error);
+  };
   return (
     <div>
-      <Form className="flex w-96 flex-col gap-4 mx-auto mt-8" >
+      {/* step-1 crate a form and make a function for on submit */}
+      <Form
+        className="flex w-96 flex-col gap-4 mx-auto mt-8"
+        onSubmit={onSubmit}
+      >
         <TextField className="w-full max-w-64" name="name" type="text">
-      <Label>Name</Label>
-      <Input placeholder="Enter your name" />
-    </TextField>
+          <Label>Name</Label>
+          <Input placeholder="Enter your name" />
+        </TextField>
         <TextField
           isRequired
           name="email"
